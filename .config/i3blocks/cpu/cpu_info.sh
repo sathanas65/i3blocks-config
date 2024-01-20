998f477daca3873cc47498cf8b9905147c67e2ee
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-20 18:52:03 -0500

    Update cpu_info.sh

--- .config/i3blocks/cpu/cpu_info.sh
+++ .config/i3blocks/cpu/cpu_info.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-TEMP=$(sensors | grep 'Package id 0:\|Tdie' | grep ':[ ]*+[0-9]*.[0-9]*°C' -o | grep '+[0-9]*.[0-9]*°C' -o)
-CPU_USAGE=$(mpstat 1 1 | awk '/Average:/ {printf("%s\n", $(NF-9))}')
-echo "$CPU_USAGE $TEMP" | awk '{ printf(" CPU:%6s% @ %s \n"), $1, $2 }'
+
+# Fetch CPU temperature
+TEMP=$(sensors | grep -o '+[0-9.]*°C' | head -n 1 | tr -d '+°C')
+
+# If no temperature data is found, set TEMP to '?°C'
+if [ -z "$TEMP" ]; then
+    TEMP='?°C'
+fi
+
+# Fetch CPU usage
+CPU_USAGE=$(mpstat 1 1 | gawk '/Average:/ {printf("%s\n", $(NF-9))}')
+
+# Output CPU usage and temperature
+echo "$CPU_USAGE $TEMP°C" | gawk '{ printf(" CPU:%6s%% @ %s \n", $1, $2) }'
